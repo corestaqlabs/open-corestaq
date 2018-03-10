40430a08cd4fe8947baa6fee8396dd5531f9f539
botfap <botfap@gmail.com>
2018-03-10 11:46:50 +0000

    add libssl-dev to guest deps

--- scripts/guest-deps.sh
+++ scripts/guest-deps.sh
@@ -1,1 +1,1 @@
-apt install build-essential git ncurses-dev python qemu-system-arm qemu-system-x86 qemu-utils
+apt install build-essential git ncurses-dev python qemu-system-arm qemu-system-x86 qemu-utils libssl-dev
