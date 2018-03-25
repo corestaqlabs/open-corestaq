1c60395486c2b96ad6fa0b90e271226f2b21aece
botfap <botfap@gmail.com>
2018-03-25 06:23:37 +0100

    Added iotfap-cloud-image target

--- scripts/guest-deps.sh
+++ scripts/guest-deps.sh
@@ -1,1 +1,1 @@
-apt install build-essential git ncurses-dev python qemu-system-arm qemu-system-x86 qemu-utils libssl-dev unzip bc qemu-system-i386
+apt install build-essential git ncurses-dev python qemu-system-arm qemu-system-x86 qemu-utils libssl-dev unzip bc qemu-system-i386 libelf-dev
