ca888dba45e7b6dae0b591eb57bf85d735303fce
Bot Fap <botfap@gmail.com>
2018-05-09 05:47:44 +0100

    Add corefs.sys (squashfs) to output
    Former-commit-id: 863884efa19dcf3a4b64ec75d7e7b7b9afd9dae3 [formerly 0402cc74b953fa169e1d22774a5660a4f5f11a56] [formerly 5d2b07d92987e0c57aaccb98f0becee576a5db08 [formerly 3f5f304f518ed18d29801ece1d2efab1d4413213]]
    Former-commit-id: 5d2b07d92987e0c57aaccb98f0becee576a5db08
    Former-commit-id: 44af66af1679180117521082410336a1ec2ce9d5

--- config/hooks/post-image.sh
+++ config/hooks/post-image.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 echo "post-image.sh"
 
+configs/board/hooks/post-image.sh
+
 rm -rf ../output/*
 
 cp output/images/rootfs.ext2 ../output/corefs.img
 
 cp output/images/rootfs.tar ../output/corefs.tar && gzip ../output/corefs.tar
 
-configs/board/hooks/post-image.sh
+cp output/images/rootfs.squashfs ../output/corefs.sys
