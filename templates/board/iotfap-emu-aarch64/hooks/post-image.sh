62e5070b3e13d638c4bcd026533db0ad405b71ab
Bot Fap <botfap@gmail.com>
2018-05-22 09:09:04 +0100

    fix iotfap-emu-aarch64 incorrect kernel image name
    Former-commit-id: eef7587a4cd38c5361458b47abdb257f6e444319
    Former-commit-id: 15e24dc47d3a65f88ff622e1ef6f18543e4f4ec8

--- templates/board/iotfap-emu-aarch64/hooks/post-image.sh
+++ templates/board/iotfap-emu-aarch64/hooks/post-image.sh
@@ -6,5 +6,5 @@ MKIMAGE=$HOST_DIR/bin/mkimage
 sudo cp output/images/Image output/bootfs/sys.k
 
 cp configs/board/output/* ../output/
-cp output/images/zImage ../output/sys.k
+cp output/images/Image ../output/sys.k
 
