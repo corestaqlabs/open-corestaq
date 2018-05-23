81f94b7f6dc66bea5b19e7966e9f9999c19b65d7
botfap <botfap@gmail.com>
2018-05-23 08:15:26 +0100

    misc: merge cleanups from botfap tree
    Former-commit-id: feeb8f4a8854888b13d739fe20a79a7ae283d1e3
    Former-commit-id: 1b598fefc35ec6518a22a15c7450ea3ac54b9fe8

--- config/hooks/post-image.sh
+++ config/hooks/post-image.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
+WHOAMI=`whoami`
+uid=$(id -u $WHOAMI)
+gid=$(id -g $WHOAMI)
+
 echo "post-image.sh"
 rm -rf ../output/*
 
 dd if=/dev/zero of=output/images/sys.conf bs=1M count=8
-mkfs.ext2 -m 0 -L configfs output/images/sys.conf
+mkfs.ext2 -m 0 -L configfs -E root_owner=$uid:$gid output/images/sys.conf
 
 dd if=/dev/zero of=output/images/bootfs.img bs=1M count=64
-mkfs.ext2 -m 0 -L bootfs output/images/bootfs.img
+mkfs.ext2 -m 0 -L bootfs -E root_owner=$uid:$gid output/images/bootfs.img
 mkdir -p output/bootfs
+
 sudo mount output/images/bootfs.img output/bootfs
 
 configs/board/hooks/post-image.sh
 
-sudo cp output/images/rootfs.squashfs output/bootfs/sys.img
-sudo cp output/images/sys.conf output/bootfs/sys.conf
+cp output/images/rootfs.squashfs output/bootfs/sys.img
+cp output/images/sys.conf output/bootfs/sys.conf
+
 sudo umount output/bootfs && sync
 
 cp output/images/rootfs.ext2 ../output/corefs.img
-cp output/images/rootfs.tar ../output/sysfs.tar && gzip ../output/sysfs.tar
+cp output/images/rootfs.tar ../output/corefs.tar && gzip ../output/corefs.tar
 cp output/images/rootfs.squashfs ../output/sys.img
 
 support/scripts/genimage.sh -c configs/board/config/sd.conf
--- scripts/deps-u18.04.sh
+++ scripts/deps-u18.04.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo apt install build-essential git ncurses-dev python qemu-system-arm qemu-system-x86 qemu-utils libssl-dev unzip bc qemu-system-i386 libelf-dev bison flex
+apt install build-essential git ncurses-dev python qemu-system-arm qemu-system-x86 qemu-utils libssl-dev unzip bc qemu-system-i386 libelf-dev bison flex pmount
+echo "/dev/loop0" >> /etc/pmount.allow
 
