a6015ee430cc7c7c7be68672d4573b49137bae41
botfap <botfap@yandex.com>
2018-07-24 05:29:38 +0100

    add: support for package group selections

--- builder/configs/board/hooks/post-build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "board template post-build.sh"
-sudo rm -rf builder/output/target/lib/modules/*
-wget -q -O - https://github.com/iotfap/oem-bsp-repo/raw/master/asus-tinker/modules-2.0.5.tgz | tar -xz -C output/target/lib/modules/
-wget -q -O - https://github.com/iotfap/oem-bsp-repo/raw/master/asus-tinker/firmware-2.0.5.tgz | tar -xz -C output/target/lib/
-
