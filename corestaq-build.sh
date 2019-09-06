b58f94165a31ce86732a0d7083ddd18304b7a021
botfap <botfap@yandex.com>
2019-09-06 01:21:46 +0100

    Enable pkgcache

--- corestaq-build.sh
+++ corestaq-build.sh
@@ -12,12 +12,12 @@ helpme () {
     echo './corestaq-build.sh build-command [device-template]'
     echo ' '
     echo 'Build Commands'
-    echo '   [s]etup - configures build system for selected device ready for use'
-    echo '   [a]ll - configures and builds all output images for selected device'
-    echo '   [d]ownload - downloads all sources for selected template'
-    echo '   [e]dit - configures selected template and edit config'
+    echo '   [s]etup [device-template] - configures build system for selected device ready for use'
+    echo '   [a]ll [device-template] - configures and builds all output images for selected device'
+    echo '   [l]inux [device-template] - builds linux kernel & modules for selected device'
+    echo '   [d]ownload [device-template] - downloads all sources for selected template'
+    echo '   [e]dit - edit current template'
     echo '   [b]uild - (re)builds current template'
-    echo '   [l]inux - builds linux kernel & modules for selected device'
     echo '   [x|xx]clean|distclean - cleans template or full build environment, board template not needed'
     echo ' '
     echo 'Available Device Templates (required):'
@@ -27,6 +27,7 @@ helpme () {
 
 activate_config () {
     source "$DEVICETEMPLATES"/"$CSQ_DEVICE"/device.conf
+    source "$PLATFORMTEMPLATES"/"$CSQ_PLATFORM"/toolchain.conf
     rm -rf "$CSQTMPL"*
     cp config/corestaq-busybox.conf buildstaq/template/
     cp scripts/build-hooks/* buildstaq/template/
@@ -38,8 +39,10 @@ activate_config () {
     cat buildstaq/template/device.conf >> buildstaq/template/corestaq_defconfig
     cat config/build.conf >> buildstaq/template/corestaq_defconfig
     
-    if [ -f config/toolchain.conf ]; then
-    	cat config/toolchain.conf >> buildstaq/template/corestaq_defconfig
+    if [ "$CSQ_TOOLCHAIN" == "local" ]; then
+    	cat config/toolchain-local.conf >> buildstaq/template/corestaq_defconfig
+    elif [ "$CSQ_TOOLCHAIN" == "external" ]; then
+    	cat config/toolchain-external.conf >> buildstaq/template/corestaq_defconfig
     else
     	cat config/toolchain-corestaq.conf >> buildstaq/template/corestaq_defconfig
     fi
@@ -59,9 +62,9 @@ activate_config () {
 
 clean_config () {
 
-	rm -rf "$CSQTMPL"*
+    rm -rf "$CSQTMPL"*
 
-    if [ $CSQBCMD = "xx" ]; then
+    if [ $CSQBCMD = "xx" ] || [ $CSQBCMD = "distclean" ]; then
         cd buildstaq && make -s clean && make -s distclean
         cd ..
     else
@@ -71,6 +74,11 @@ clean_config () {
 }
 
 download_config () {
+    if [ -d pkgcache/src/dl ] && [ ! -d buildstaq/dl ]; then
+        echo "Copying local pkgcache"
+	cp -Rvf pkgcache/src/dl buildstaq/
+    fi
+
     cd buildstaq && make -s source
     cd ..
 }
@@ -111,12 +119,8 @@ if [ -f "$DEVICETEMPLATES"/"$CSQ_DEVICE"/device.conf ]; then
             activate_config
             download_config
             ;;
-        e|edit)
-            activate_config
-            edit_config
-            ;;
         l|linux)
-        	activate_config
+            activate_config
             make_linux
             ;;
         *)
@@ -125,11 +129,14 @@ if [ -f "$DEVICETEMPLATES"/"$CSQ_DEVICE"/device.conf ]; then
     esac
 else
     case "$CSQBCMD" in
-        x|xx|clean)
+        x|xx|clean|distclean)
             clean_config
         ;;
         b|build)
             make_template
+            ;;
+        e|edit)
+            edit_config
             ;;
         *)
             helpme
